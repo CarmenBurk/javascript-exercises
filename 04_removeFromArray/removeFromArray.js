8bde04f5eca2082c7e85675bcfbcefbf0a107363
CarmenBurk~ <xccarmen@gmail.com>
2022-09-23 21:26:28 -0400

    finished exercise 4, learned that for of loops stink

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
-};
+    const removeFromArray = function(array,...removedValues) {
+       
+        let newArray = array
+        const removedArray = removedValues
+        console.log(removedArray)
+        for (index = 0; index < array.length; index++){
+            let value = array[index]
+            for (const removedValue of removedArray) {
+                console.log("to be removed: "+ removedValue)
+                if (value == removedValue){
+                console.log(value + " is equal to " + removedValue +" and the current index is "+index)
+                newArray.splice(index,1)
+                index --;
+               }
+                
+            }
+            
+        }
+    return newArray;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
